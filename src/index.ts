513800741ec6e39f994919ff3feb3006b4f29fb4
windbullet <2536810643@qq.com>
2023-10-24 12:25:40 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,33 @@ export const name = 'at-broadcast'
 export const using = ['database']
 
 export const usage = `
+## 如果你是旧版本用户，需要在数据库中的broadcastData表中向空的group字段中添加组名，否则将会遇到bug
+
 群聊使用方法：
 
-    订阅广播 有广播时会被at
+> 创建订阅分组 <组名>
+>> 需要超级管理员  
+>> 创建一个分组，广播时按分组广播
 
-    取消订阅广播 取消订阅广播（不然呢？）
+> 删除订阅分组 <组名>
+>> 需要超级管理员  
+>> 删除一个分组
 
-私聊使用方法（需要超级管理员，可在配置页设置）：
+> 订阅广播 <组名>  
+>> 有广播时会被at
 
-    广播 <群号> <广播内容> 向指定群聊广播
+> 取消订阅广播 <组名>  
+>> 取消订阅这个分组的广播
 
-    全域广播 <广播内容> 向所有群广播
+私聊使用方法：  
+
+> 广播 <群号> <组名> <广播内容>  
+>> 需要超级管理员  
+>> 向指定群聊的指定分组广播
+
+> 全域广播 <组名> <广播内容>  
+>> 需要超级管理员  
+>> 向所有群的指定分组广播
 `
 
 declare module 'koishi' {
@@ -28,6 +44,7 @@ export interface BroadcastData {
   id: number;
   guildId: string;
   userId: string[];
+  group: string;
 }
 
 export interface Config {
@@ -41,76 +58,128 @@ export const Config: Schema<Config> = Schema.object({
 
 export function apply(ctx: Context, config: Config) {
   extendTable(ctx)
-  ctx.guild().command("订阅广播", "有广播时你将会被at")
-    .action(async ({session}) => {
+  ctx.command("广播", "订阅制广播")
+
+  ctx.guild().command("广播").subcommand(".创建订阅分组 <name:string>", "创建一个分组，广播时按分组广播", {checkArgCount: true}).alias("创建订阅分组")
+    .example("广播.创建订阅分组 Koishi更新通知")
+    .action(async ({session}, name) => {
+      if (config.超级管理员.includes(session.event.user.id)) {
+        let data = await ctx.database.get("broadcastData", {
+          guildId: session.event.guild.id,
+          group: name,
+        })
+        if (data.length !== 0) {
+          return h("quote", {id: session.event.message.id}) + "该分组已存在"
+        } else {
+          await ctx.database.create("broadcastData", {
+            guildId: session.event.guild.id,
+            group: name,
+            userId: [],
+          })
+          return h("quote", {id: session.event.message.id}) + "订阅分组创建成功"
+        }
+      } 
+      return h("quote", {id: session.event.message.id}) + "你没有权限"
+    })
+
+  ctx.guild().command("广播").subcommand(".删除订阅分组 <group:string>", "删除一个分组", {checkArgCount: true}).alias("删除订阅分组")
+    .example("广播.删除订阅分组 Koishi更新通知")
+    .action(async ({session}, group) => {
+      if (config.超级管理员.includes(session.event.user.id)) {
+        let data = await ctx.database.get("broadcastData", {
+          guildId: session.event.guild.id,
+          group: group,
+        })
+        if (data.length === 0) {
+          return h("quote", {id: session.event.message.id}) + "该分组不存在"
+        }
+        await ctx.database.remove("broadcastData", {
+          guildId: session.event.guild.id,
+          group: group,
+        })
+        return h("quote", {id: session.event.message.id}) + "删除订阅分组成功"
+      } 
+      return h("quote", {id: session.event.message.id}) + "你没有权限"
+    })
+
+  ctx.guild().command("广播").subcommand(".订阅广播 <group:string>", "订阅的分组有广播时你将会被at", {checkArgCount: true}).alias("订阅广播")
+    .action(async ({session}, group) => {
       let data = await ctx.database.get("broadcastData", {
         guildId: session.event.guild.id,
+        group: group,
       });
 
       if (data.length === 0) {
-        await ctx.model.create("broadcastData", {
-          guildId: session.event.guild.id,
-          userId: [session.event.user.id],
-        })
-        return h("quote", {id: session.event.message.id}) + "订阅成功"
-      } else if (!data[0].userId.includes(session.event.user.id)) {
+        return h("quote", {id: session.event.message.id}) + "该分组不存在"
+      } else if (data[0].userId.includes(session.event.user.id)) {
+        return h("quote", {id: session.event.message.id}) + "你已经订阅了该分组"
+      } else {
         data[0].userId.push(session.event.user.id)
-        await ctx.model.set("broadcastData", {guildId: session.event.guild.id}, {
+        await ctx.model.set("broadcastData", {guildId: session.event.guild.id, group: group}, {
           userId: data[0].userId,
         })
         return h("quote", {id: session.event.message.id}) + "订阅成功"
       }
-      return h("quote", {id: session.event.message.id}) + "你已订阅过"
       
     })
 
-  ctx.guild().command("取消订阅广播")
-    .action(async ({session}) => {
+  ctx.guild().command("广播").subcommand(".取消订阅广播 <group:string>", {checkArgCount: true}).alias("取消订阅广播")
+    .action(async ({session}, group) => {
       let data = await ctx.database.get("broadcastData", {
         guildId: session.event.guild.id,
+        group: group,
       });
-      if (data.length === 0 || !data[0].userId.includes(session.event.user.id)) {
-        return h("quote", {id: session.event.message.id}) + "你还未订阅"
+      if (data.length === 0) {
+        return h("quote", {id: session.event.message.id}) + "该分组不存在"
+      } else if (!data[0].userId.includes(session.event.user.id)) {
+        return h("quote", {id: session.event.message.id}) + "你没有订阅该分组"
+      } else {
+        data[0].userId.splice(data[0].userId.indexOf(session.event.user.id), 1)
+        await ctx.model.set("broadcastData", {guildId: session.event.guild.id, group: group}, {
+          userId: data[0].userId,
+        })
+        return h("quote", {id: session.event.message.id}) + "取消订阅成功"
       }
-      data[0].userId.splice(data[0].userId.indexOf(session.event.user.id), 1)
-      await ctx.model.set("broadcastData", {guildId: session.event.guild.id}, {
-        userId: data[0].userId,
-      })
-      return h("quote", {id: session.event.message.id}) + "已取消订阅"
+      
     })
 
-  ctx.private().command("广播 <guildId:string> <message:text>", "向指定群广播消息")
-    .example("广播 114514 Koishi更新了5.14.1版本")
-    .action(async ({session}, guildId, message) => {
+  ctx.private().command("广播").subcommand(".发起广播 <guildId:string> <group:string> <message:text>", "向指定群的指定分组广播消息", {checkArgCount: true}).alias("发起广播")
+    .example("广播.发起广播 114514 Koishi更新通知 Koishi更新了5.14.1版本")
+    .action(async ({session}, guildId, group, message) => {
       if (config.超级管理员.includes(session.event.user.id)) {
         let result = ""
         let data = await ctx.database.get("broadcastData", {
           guildId: guildId,
+          group: group,
         });
+
         if (data.length === 0) {
-          return h("quote", {id: session.event.message.id}) + "该群还没有人订阅广播"
+          return h("quote", {id: session.event.message.id}) + "该分组不存在"
+        } else if (data[0].userId.length === 0) {
+          return h("quote", {id: session.event.message.id}) + "该分组没有订阅者"
         }
+
         for (let i of data[0].userId) {
           result += `<at id="${+i}"/>`
         }
         result += ` ${message}`
         session.bot.sendMessage(guildId, result)
-        return h("quote", {id: session.event.message.id}) + `群聊：${guildId} 广播成功`
+        return h("quote", {id: session.event.message.id}) + `群聊：${guildId}\n分组：${group}\n广播成功`
       }
       return h("quote", {id: session.event.message.id}) + "你没有权限"
     })
 
-  ctx.private().command("全域广播 <message:text>", "向所有群广播消息")
-    .example("全域广播 Koishi更新了5.14.1版本")
-    .action(async ({session}, message) => {
+  ctx.private().command("广播").subcommand(".全域广播 <group:string> <message:text>", "向所有群的指定分组广播消息", {checkArgCount: true}).alias("全域广播")
+    .example("广播.全域广播 Koishi更新了5.14.1版本")
+    .action(async ({session}, group, message) => {
       if (config.超级管理员.includes(session.event.user.id)) {
         let guilds = ["收到广播的群聊："]
         let data = await ctx.database
         .select("broadcastData")
-        .orderBy("id", "desc")
+        .where({group: group})
         .execute()
         if (data.length === 0) {
-          return h("quote", {id: session.event.message.id}) + "还没有人订阅广播"
+          return h("quote", {id: session.event.message.id}) + "所有群都不存在该分组"
         }
         for (let i of data) {
           let result = ""
@@ -133,5 +202,6 @@ async function extendTable(ctx) {
     id: "unsigned",
     guildId: "text",
     userId: "list",
+    group: "text",
   }, {autoInc: true, primary: "id"})
 }
